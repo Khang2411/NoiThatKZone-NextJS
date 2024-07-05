48bfc77824123c2fa802825a16862012df6d641c
Khang2411 <46453014+Khang2411@users.noreply.github.com>
2024-07-06 05:01:59 +0700

    Update axios-client.ts

--- api-client/axios-client.ts
+++ api-client/axios-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://admin.noithatkzone.shop/api',
     headers: { 'Content-Type': 'application/json' }
 });
 
