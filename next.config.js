ad4d0d1878ba4fe273756bf61ec87101fcb9e1ee
Khang2411 <46453014+Khang2411@users.noreply.github.com>
2024-07-06 07:57:40 +0700

    Update next.config.js proxy

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
             {
                 source: "/api/auth/:path*",
                 destination: "/api/auth/:path*"
-            }
-            // {
-            //     source: '/api/:path*',
-            //     destination: `${process.env.API_URL}/api/:path*`, // Proxy to Backend
-            // },
+            },
+             {
+                 source: '/api/:path*',
+                 destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`, // Proxy to Backend
+             }
         ]
     },
     images: {
