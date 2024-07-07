ccbae4fb4a215174645fda74d80826daf705a563
Khang2411 <46453014+Khang2411@users.noreply.github.com>
2024-07-07 11:51:54 +0700

    Update next.config.js remove env

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
             },
              {
                  source: '/api/:path*',
-                 destination: `https://admin.noithatkzone.shop/api/:path*`, // Proxy to Backend
+                 destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`, // Proxy to Backend
              }
         ]
     },
